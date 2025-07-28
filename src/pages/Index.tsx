892c398a2ea57fd14589718aaeb9349486dda7c9
RaviRaj716 <ravirangu12@gmail.com>
2025-07-28 07:06:36 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -118,7 +118,7 @@ const Index = () => {
           </div>
         </div>
         {/* Ad Image Section - Mobile Only */}
-        <div className="max-w-4xl mx-auto mb-2 md:hidden">
+        <div className="max-w-4xl mx-auto mb-4 md:hidden">
           <a 
             href="https://palmai.live" 
             target="_blank" 
